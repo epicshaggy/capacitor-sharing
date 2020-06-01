21ba0a68e74469fdbc0e29cfc1209850be600a2d
Jose Martinez <jmartinez@Joses-MacBook-Pro-2.local>
2020-06-01 18:19:41 -0400

    Added web implementation

--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,8 @@
 import { WebPlugin } from "@capacitor/core";
 import { SharingPlugin, SharingOptions } from "./definitions";
 
+import * as FileSaver from "file-saver";
+
 export class SharingWeb extends WebPlugin implements SharingPlugin {
   constructor() {
     super({
@@ -9,9 +11,25 @@ export class SharingWeb extends WebPlugin implements SharingPlugin {
     });
   }
 
+  toByteArray(base64Data: string): Uint8Array {
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    return new Uint8Array(byteNumbers);
+  }
+
   async share(options: SharingOptions): Promise<any> {
-    console.log("ECHO", options);
-    return Promise.resolve();
+    return new Promise<any>((resolve) => {
+      for (let i = 0; i < options.fileNames.length; i++) {
+        let blob = new Blob([this.toByteArray(options.base64Values[i])], {
+          type: "application/octet-stream",
+        });
+        FileSaver.saveAs(blob, options.fileNames[i]);
+      }
+      resolve();
+    });
   }
 }
 
