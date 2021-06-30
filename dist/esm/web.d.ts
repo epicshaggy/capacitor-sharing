8e3ddd383c191e54e0f3a2dca2e12bdc99b8d808
Jose Martinez <epicshaggy@gmail.com>
2021-06-30 15:36:43 -0400

    preparing to move to a different branch dedicated for maintenance with cap 2

--- dist/esm/web.d.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from "@capacitor/core";
-import { SharingPlugin, SharingOptions } from "./definitions";
-export declare class SharingWeb extends WebPlugin implements SharingPlugin {
-    constructor();
-    toByteArray(base64Data: string): Uint8Array;
-    share(options: SharingOptions): Promise<any>;
-}
-declare const Sharing: SharingWeb;
-export { Sharing };
